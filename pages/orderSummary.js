b6e7a4a7e68a239ddc67ab8d487b31fc41a73a47
Danny Nguyen <dnguyen130@my.bcit.ca>
2021-11-03 22:39:08 -0700

    started order summary + topheader

---
+++ pages/orderSummary.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View , SafeAreaView, ScrollView, ImageBackground} from 'react-native';
+
+import BigButton from '../comps/BigButton';
+import Input from '../comps/Input';
+import SmallButton from '../comps/SmallButton';
+import Title from '../comps/Title';
+import TextDivider from '../comps/TextDivider';
+import styled from 'styled-components';
+
+const
