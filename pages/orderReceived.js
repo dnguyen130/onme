f33c735edc36721888c874bc3917ea1c802c300b
Danny Nguyen <dnguyen130@my.bcit.ca>
2021-11-16 22:37:32 -0800

    updated directory

--- pages/orderReceived.js
+++ pages/orderReceived.js
@@ -2,45 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 
-<<<<<<< HEAD
 import BigButton from '../components/buttons/BigButton';
 import NavBar from '../components/global/NavBar';
 import Header from '../components/global/Header';
 import FloatingBox from '../components/summary/FloatingBox';
-=======
-import BigButton from '../comps/BigButton';
-import Input from '../comps/Input';
-import SmallButton from '../comps/SmallButton';
-import NavBar from '../comps/NavBar';
-import Title from '../comps/Title';
-import TextLink from '../comps/TextLink';
-import TextDivider from '../comps/TextDivider';
-import Card from '../comps/Card'
-import styled from 'styled-components';
-import Header from '../comps/Header';
-import FloatingBox from '../comps/FloatingBox';
-
-export default function OrderConfirmation({navigation}) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <Header subTitle="Order Confirmation" subWeight="700" />
-      <FloatingBox 
-        boxHeight="40%" 
-        boxWidth="70%" 
-        mainTitle="Awesome, Order Received!" mainSize="15px"
-        subTitle="We hope you get some :)" subSize="15px" 
-      />
-      <BigButton buttonText="Send Another :)" onPress={() => navigation.navigate('Restaurant Menu Drinks')} />
-      <NavBar 
-        homeOnPress={() => navigation.navigate('Dashboard')}
-        midOnPress={() => navigation.navigate('Restaurant Menu Drinks')}
-        settingsOnPress={() => navigation.navigate('Settings')}
-      />
-    </SafeAreaView>
-  );
-}
->>>>>>> f9a3846ef917a3fc3e75e83cab39ef783e4bd4cd
 
 const styles = StyleSheet.create({
   container: {
--- pages/recentItems.js
+++ pages/recentItems.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-<<<<<<< HEAD
 import { StyleSheet, SafeAreaView } from 'react-native';
 import styled from 'styled-components/native';
 
@@ -8,15 +7,6 @@ import LocButton from '../components/buttons/LocButton';
 import Input from '../components/global/Input';
 import NavBar from '../components/global/NavBar';
 import Title from '../components/text/Title';
-=======
-import { StyleSheet, SafeAreaView, ScrollView, View } from 'react-native';
-
-import LocButton from '../comps/LocButton';
-import Input from '../comps/Input';
-import NavBar from '../comps/NavBar';
-import Header from '../comps/Header';
-import styled from 'styled-components';
->>>>>>> f9a3846ef917a3fc3e75e83cab39ef783e4bd4cd
 
 const ScrollCont = styled.View`
   flex: 1;
@@ -35,7 +25,6 @@ const NavBarCont = styled.View`
   right: 0;
 `;
 
-<<<<<<< HEAD
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -46,14 +35,6 @@ const styles = StyleSheet.create({
 });
 
 export default function RecentItems({navigation}) {
-=======
-const InputCont = styled.View`
-  padding-bottom: 5%;
-  top: -4%;
-`;
-
-export default function FrequentlyVisited({navigation}) {
->>>>>>> f9a3846ef917a3fc3e75e83cab39ef783e4bd4cd
   return (
     <SafeAreaView style={styles.container}>
         <StatusBar style="auto" />
@@ -91,15 +72,4 @@ export default function FrequentlyVisited({navigation}) {
       </NavBarCont>
     </SafeAreaView>
   );
-<<<<<<< HEAD
 }
-=======
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2E2E2E',
-  }
-});
->>>>>>> f9a3846ef917a3fc3e75e83cab39ef783e4bd4cd
