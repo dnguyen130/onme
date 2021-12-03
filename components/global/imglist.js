85985bc17c5f01f09eca230b2b335987045ff40e
Danny Nguyen <dnguyen130@my.bcit.ca>
2021-12-03 00:13:56 -0800

    updated restaurant imgs

--- components/global/imglist.js
+++ components/global/imglist.js
@@ -1,4 +1,8 @@
 export const MyImages = {
-  SW01: require('../../assets/images/header/headerImg.png'),
-  Habitat: require('../../assets/images/restaurant/habitat.png')
+  SW01: require('../../assets/images/restaurant/sw01.jpg'),
+  Habitat: require('../../assets/images/restaurant/habitat.png'),
+  Browns: require('../../assets/images/restaurant/browns.jpg'),
+  Keg: require('../../assets/images/restaurant/keg.jpg'),
+  Joey: require('../../assets/images/restaurant/joey.jpg'),
+  Cactus: require('../../assets/images/restaurant/cactus.jpg')
 }
